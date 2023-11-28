965a08ca9d70a52bf401d3d618c4a3d4213027e8
tomithediamond <missywhizzy@gmail.com>
2023-11-28 22:33:57 +0100

    an image gallery

---
+++ README.md
@@ -0,0 +1,18 @@
+## PROJECT TITLE:
+HerTechTrail Academy Web Development Pro Track Fitness Test
+
+## CONTENTS
+This is a picture gallery containing images of the 17 Sustainable Development Goals (SDGs) and a name under each picture. On clicking on each name, the names and the background change colors.
+
+## TECHNOLOGIES USED
+HTML5
+CSS3 
+JavaScript
+Bootstrap 5.1
+
+## LINKS 
+My Github repository *https://github.com/tomithediamond/Image_Gallery*
+Hosted page 
+
+## ACKNOWLEDGEMENT 
+I thank Hertechtrail Academy for this partform and the opportunity to grow in the field.
---
+++ index.js
@@ -0,0 +1,119 @@
+const modify = document.getElementById("sdg1");
+
+modify.addEventListener("click", (event) => {
+    event.target.style.color = "saddlebrown";
+    event.target.style.background = "lightgreen";
+});
+
+const modify2 = document.getElementById("sdg2");
+
+modify2.addEventListener("click", (event) => {
+    event.target.style.color = "white";
+    event.target.style.background = "purple";
+});
+
+const modify3 = document.getElementById("sdg3");
+
+modify3.addEventListener("click", (event) => {
+    event.target.style.color = "peachpuff";
+    event.target.style.background = "black";
+});
+
+const modify4 = document.getElementById("sdg4");
+
+modify4.addEventListener("click", (event) => {
+    event.target.style.color = "black";
+    event.target.style.background = "red";
+});
+
+const modify5 = document.getElementById("sdg5");
+
+modify5.addEventListener("click", (event) => {
+    event.target.style.color = "violet";
+    event.target.style.background = "yellow";
+});
+
+const modify6 = document.getElementById("sdg6");
+
+modify6.addEventListener("click", (event) => {
+    event.target.style.color = "indigo";
+    event.target.style.background = "orange";
+});
+
+const modify7 = document.getElementById("sdg7");
+
+modify7.addEventListener("click", (event) => {
+    event.target.style.color = "green";
+    event.target.style.background = "gray";
+});
+
+const modify8 = document.getElementById("sdg8");
+
+modify8.addEventListener("click", (event) => {
+    event.target.style.color = "wine";
+    event.target.style.background = "green";
+});
+
+const modify9 = document.getElementById("sdg9");
+
+modify9.addEventListener("click", (event) => {
+    event.target.style.color = "blue";
+    event.target.style.background = "white";
+});
+
+const modify10 = document.getElementById("sdg10");
+
+modify10.addEventListener("click", (event) => {
+    event.target.style.color = "white";
+    event.target.style.background = "lightgreen";
+});
+
+const modify11 = document.getElementById("sdg11");
+
+modify11.addEventListener("click", (event) => {
+    event.target.style.color = "lightskyblue";
+    event.target.style.background = "black";
+});
+
+const modify12 = document.getElementById("sdg12");
+
+modify12.addEventListener("click", (event) => {
+    event.target.style.color = "black";
+    event.target.style.background = "grey";
+});
+
+const modify13 = document.getElementById("sdg13");
+
+modify13.addEventListener("click", (event) => {
+    event.target.style.color = "white";
+    event.target.style.background = "purple";
+});
+
+const modify14 = document.getElementById("sdg14");
+
+modify14.addEventListener("click", (event) => {
+    event.target.style.color = "red";
+    event.target.style.background = "blue";
+});
+
+const modify15 = document.getElementById("sdg15");
+
+modify15.addEventListener("click", (event) => {
+    event.target.style.color = "peachpuff";
+    event.target.style.background = "lightgreen";
+});
+
+const modify16 = document.getElementById("sdg16");
+
+modify16.addEventListener("click", (event) => {
+    event.target.style.color = "green";
+    event.target.style.background = "orange";
+});
+
+const modify17 = document.getElementById("sdg17");
+
+modify17.addEventListener("click", (event) => {
+    event.target.style.color = "white";
+    event.target.style.background = "brown";
+});
+
